bc672d0f731cdf5c483a9ad4abf0af6c3b766c6f
Steven Sherry <steven.r.sherry@gmail.com>
2019-05-02 21:49:43 -0500

    Replace UIKit import with Foundation for SPM

--- SwiftyRequest/SwiftyRequest.h
+++ SwiftyRequest/SwiftyRequest.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Steven Sherry. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftyRequest.
 FOUNDATION_EXPORT double SwiftyRequestVersionNumber;
