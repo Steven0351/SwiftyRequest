fa495856ea0514a734ba722ce247ecb384235f28
Steven Sherry <steven.r.sherry@gmail.com>
2019-05-09 21:46:46 -0500

    Implemented SPM support. Succesfully builds and tests for Mac targets.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
 // swift-tools-version:5.0
 import PackageDescription
+import Foundation
+
+func sources(at relativePath: String) -> [String] {
+  let currentDirectory = String(cString: getenv("PWD"))
+    return try! FileManager.default
+      .contentsOfDirectory(atPath: "\(currentDirectory)/\(relativePath)")
+      .filter { $0.contains(".swift") }
+}
+
+let librarySources = sources(at: "SwiftyRequest")
+let testSources = sources(at: "SwiftyRequestTests")
 
 let package = Package(
   name: "SwiftyRequest",
@@ -9,12 +20,14 @@ let package = Package(
   targets: [
     .target(
       name: "SwiftyRequest",
-      path: "SwiftyRequest"
+      path: "SwiftyRequest",
+      sources: librarySources
     ),
     .testTarget(
       name: "SwiftyRequestTests",
       dependencies: ["SwiftyRequest"],
-      path: "SwiftyRequestTests"
+      path: "SwiftyRequestTests",
+      sources: testSources
     )
   ]
 )
